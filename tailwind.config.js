d836ced289339117dd90ff011cd3e18b2df988eb
Aleksei Revin <alex@jaralex.ee>
2023-03-02 19:42:48 +0200

    chore: played with fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -28,17 +28,14 @@ module.exports = {
       black: "#000000",
       yellow: "#f9e71c",
       lila: "#e6e5ec",
+      'lila-darker': "#d7d5e1",
       "grey-10": "#6c6b6d",
       "grey-20": "#7c7c7c",
       "grey-30": "#919091",
       "grey-40": "#929293",
       "grey-50": "#f4f3f8",
       "grey-60": "#edebf6",
-      "grey-70": "#d8d8d8",
-      "hero-gradient-from": "rgba(85, 64, 174, 0.95)",
-      "hero-gradient-to": "rgba(65, 47, 144, 0.93)",
-      "blog-gradient-from": "#8f9098",
-      "blog-gradient-to": "#222222",
+      "grey-70": "#d8d8d8"
     },
 
     container: {
